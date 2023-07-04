64282f1d3eb288ddf578966a751c801323816c74
Reza D. Pradipta <rezhakechenk67@gmail.com>
2023-07-05 05:21:47 +0700

    Add files via upload

---
+++ rm this and that/changeNumToEdu.py
@@ -0,0 +1,24 @@
+category_dict = {
+    4:  "SD",
+    5:  "SMP",
+    6:  "SMA",
+    7:  "D1",
+    8:  "D2",
+    9:  "D3",
+    10: "S1",
+    11: "S2",
+    12: "S3"
+}
+
+input_file = "cekulah.txt"  # Path to the input text file with numbers
+output_file = "sekolah.txt"  # Path to the output text file to save category labels
+
+with open(input_file, "r") as file:
+    numbers = [int(line.strip()) for line in file.readlines()]
+
+labels = [category_dict.get(num, "Unknown") for num in numbers]
+
+with open(output_file, "w") as file:
+    file.write("\n".join(labels))
+
+print("Category labels saved in", output_file)
---
+++ rm this and that/changeNumToWork.py
@@ -0,0 +1,28 @@
+category_dict = {
+    1: "PELAJAR",
+    2: "MAHASISWA",
+    3: "KARYAWAN SWASTA",
+    4: "PNS",
+    5: "ASN",
+    6: "FREELANCER",
+    7: "WIRAUSAHA",
+    8: "IBU RUMAH TANGGA",
+    9: "LAINNYA",
+    10: "Guru",
+    11: "Dosen",
+    12: "Pelajar SMK",
+    13: "Guru SMK"
+}
+
+input_file = "kerjo.txt"  # Path to the input text file with numbers
+output_file = "kerjoan.txt"  # Path to the output text file to save category labels
+
+with open(input_file, "r") as file:
+    numbers = [int(line.strip()) for line in file.readlines()]
+
+labels = [category_dict.get(num, "Unknown") for num in numbers]
+
+with open(output_file, "w") as file:
+    file.write("\n".join(labels))
+
+print("Category labels saved in", output_file)
